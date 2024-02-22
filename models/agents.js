2ab5a7d987378c9d19fffd3b8767ca68c0d3ab18
CV2Tre <cvlookin@me.com>
2024-02-21 22:56:54 -0500

    changes

--- models/agents.js
+++ models/agents.js
@@ -6,7 +6,9 @@ const Agent = new Schema(
         name: { type: String, required: true },
         address: { type: String, required: true },
         email: { type: String, required: true },
-        phone: { type: String, required: true }
+        phone: { type: String, required: true },
+        headshotImgPath: { type: String, required: false, default: "" },
+        properties: { type: Array, required: false, default: [] }
     },
     { timestamps: true }
 );
